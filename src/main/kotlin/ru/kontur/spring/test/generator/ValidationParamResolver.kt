2e4cb97172e1486c7ccaabe55c2e0d1cd5b4689d
kostya05983 <kostya05983@mail.ru>
2019-10-06 01:08:26 +0700

    Some changes in signature

--- src/main/kotlin/ru/kontur/spring/test/generator/ValidationParamResolver.kt
+++ src/main/kotlin/ru/kontur/spring/test/generator/ValidationParamResolver.kt
@@ -1,9 +1,12 @@
 package ru.kontur.spring.test.generator
 
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
 /**
  * @author Konstantin Volivach
  */
-interface ValidationParamResolver<T> {
+interface ValidationParamResolver {
 
-    fun process(param: T): T
+    fun <T> process(param: T, clazz: KClass<*>, type: KType): T
 }
