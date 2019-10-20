4a4600942cfba59ba79c9b4cc19327252452d003
kostya05983 <kostya05983@mail.ru>
2019-10-20 21:45:53 +0700

    Add notBlankGenerator

---
+++ src/main/kotlin/ru/kontur/spring/test/generator/generators/NotBlankGenerator.kt
@@ -0,0 +1,26 @@
+package ru.kontur.spring.test.generator.generators
+
+import ru.kontur.spring.test.generator.api.ValidationParamResolver
+import ru.kontur.spring.test.generator.utils.generateString
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
+/**
+ * @author Konstantin Volivach
+ */
+class NotBlankGenerator : ValidationParamResolver {
+    private companion object {
+        const val DEFAULT_SIZE = 10
+    }
+
+    override fun <T> process(generatedParam: T?, clazz: KClass<*>, type: KType): Any {
+        when (clazz) {
+            String::class -> {
+                if (generatedParam == null || generatedParam is String && generatedParam.isBlank()) {
+                    return generateString(DEFAULT_SIZE)
+                }
+            }
+        }
+        return generatedParam!!
+    }
+}
