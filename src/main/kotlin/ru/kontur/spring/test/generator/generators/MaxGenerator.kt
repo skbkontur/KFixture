88880eec824f79b3314258edda733f56c1a3cf91
kostya05983 <kostya05983@mail.ru>
2019-10-20 21:41:06 +0700

    MaxGenerator

---
+++ src/main/kotlin/ru/kontur/spring/test/generator/generators/MaxGenerator.kt
@@ -0,0 +1,50 @@
+package ru.kontur.spring.test.generator.generators
+
+import ru.kontur.spring.test.generator.api.ValidationParamResolver
+import java.math.BigDecimal
+import java.math.BigInteger
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
+/**
+ * @author Konstantin Volivach
+ */
+class MaxGenerator(
+    private val max: Long
+) : ValidationParamResolver {
+    override fun <T> process(generatedParam: T?, clazz: KClass<*>, type: KType): Any {
+        when (clazz) {
+            BigDecimal::class -> {
+                if (generatedParam == null || generatedParam is BigDecimal && generatedParam > BigDecimal(max)) {
+                    return BigDecimal(max - 1)
+                }
+            }
+            BigInteger::class -> {
+                if (generatedParam == null || generatedParam is BigInteger && generatedParam > BigInteger.valueOf(max)) {
+                    return BigInteger.valueOf(max - 1)
+                }
+            }
+            Byte::class -> {
+                if (generatedParam == null || generatedParam is Byte && generatedParam > max) {
+                    return (max - 1).toByte()
+                }
+            }
+            Short::class -> {
+                if (generatedParam == null || generatedParam is Short && generatedParam > max) {
+                    return (max - 1).toShort()
+                }
+            }
+            Int::class -> {
+                if (generatedParam == null || generatedParam is Int && generatedParam > max) {
+                    return (max - 1).toInt()
+                }
+            }
+            Long::class -> {
+                if (generatedParam == null || generatedParam is Long && generatedParam > max) {
+                    return max - 1
+                }
+            }
+        }
+        return generatedParam!!
+    }
+}
