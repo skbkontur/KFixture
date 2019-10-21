d9f5b73c40521dd46e26c632e90349c5a1cd1415
kostya05983 <kostya05983@mail.ru>
2019-10-21 17:54:34 +0700

    Add assertTrue and assert false

---
+++ src/main/kotlin/ru/kontur/spring/test/generator/generators/AssertFalseGenerator.kt
@@ -0,0 +1,11 @@
+package ru.kontur.spring.test.generator.generators
+
+import ru.kontur.spring.test.generator.api.ValidationParamResolver
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
+class AssertFalseGenerator : ValidationParamResolver {
+    override fun <T> process(generatedParam: T?, clazz: KClass<*>, type: KType): Any {
+        return false
+    }
+}
