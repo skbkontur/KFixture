e42b2acd51cde46b469c602260392b701a2306b4
kostya05983 <kostya05983@mail.ru>
2019-10-26 20:14:20 +0700

    Others generators

---
+++ src/main/kotlin/ru/kontur/spring/test/generator/generators/PastOrPresentGenerator.kt
@@ -0,0 +1,11 @@
+package ru.kontur.spring.test.generator.generators
+
+import ru.kontur.spring.test.generator.api.ValidationParamResolver
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
+class PastOrPresentGenerator: ValidationParamResolver {
+    override fun <T> process(generatedParam: T?, clazz: KClass<*>, type: KType): Any {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
