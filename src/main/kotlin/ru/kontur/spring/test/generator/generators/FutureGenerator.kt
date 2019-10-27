5b6903b141439b24a1217d5e5a640e942fa7be1f
kostya05983 <kostya05983@mail.ru>
2019-10-27 23:39:40 +0700

    Some base for futureGenerator

--- src/main/kotlin/ru/kontur/spring/test/generator/generators/FutureGenerator.kt
+++ src/main/kotlin/ru/kontur/spring/test/generator/generators/FutureGenerator.kt
@@ -1,12 +1,72 @@
 package ru.kontur.spring.test.generator.generators
 
 import ru.kontur.spring.test.generator.api.ValidationParamResolver
+import java.time.*
+import java.time.chrono.HijrahDate
+import java.time.chrono.JapaneseDate
+import java.time.chrono.MinguoDate
+import java.time.chrono.ThaiBuddhistDate
+import java.time.temporal.ChronoUnit
+import java.util.*
+import javax.validation.constraints.Future
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
 
 class FutureGenerator : ValidationParamResolver {
 
     override fun <T> process(generatedParam: T?, clazz: KClass<*>, type: KType, annotation: Annotation): Any {
+        val futureAnnotation = annotation as Future
+
+        when (clazz) {
+            Date::class -> {
+                return Date.from(Instant.now().plus(1000, ChronoUnit.HOURS))
+            }
+            Calendar::class -> {
+                return Calendar.getInstance() //TODO
+            }
+            Instant::class -> {
+
+            }
+            LocalDate::class -> {
+
+            }
+            LocalDateTime::class -> {
+
+            }
+            LocalTime::class -> {
+
+            }
+            MonthDay::class -> {
+
+            }
+            OffsetDateTime::class -> {
+
+            }
+            OffsetTime::class -> {
+
+            }
+            Year::class -> {
+
+            }
+            YearMonth::class -> {
+
+            }
+            ZonedDateTime::class -> {
+
+            }
+            HijrahDate::class -> {
+
+            }
+            JapaneseDate::class -> {
+
+            }
+            MinguoDate::class -> {
+
+            }
+            ThaiBuddhistDate::class -> {
+
+            }
+        }
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
