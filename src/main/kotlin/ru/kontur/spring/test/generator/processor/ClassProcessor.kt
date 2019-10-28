f77912537252535ad767b3a8876a801570f9eae4
kostya05983 <kostya05983@mail.ru>
2019-10-29 02:13:11 +0700

    Update clazzProcessor for many annotations

--- src/main/kotlin/ru/kontur/spring/test/generator/processor/ClassProcessor.kt
+++ src/main/kotlin/ru/kontur/spring/test/generator/processor/ClassProcessor.kt
@@ -8,6 +8,7 @@ import ru.kontur.spring.test.generator.exceptions.NoSuchValidAnnotationException
 import ru.kontur.spring.test.generator.generators.*
 import ru.kontur.spring.test.generator.utils.makeRandomInstance
 import ru.kontur.spring.test.generator.utils.toKType
+import java.lang.RuntimeException
 import javax.validation.constraints.*
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
@@ -17,62 +18,99 @@ import kotlin.reflect.KType
  */
 class ClassProcessor {
     private val generators: Map<KClass<out Annotation>, ValidationParamResolver> = mapOf(
-        AssertFalse::class to AssertFalseGenerator(),
-        AssertTrue::class to AssertTrueGenerator(),
-        DecimalMax::class to DecimalMaxGenerator(),
-        DecimalMin::class to DecimalMinGenerator(),
-        Digits::class to DigitsGenerator(),
-        Email::class to EmailGenerator(),
-        Future::class to FutureGenerator(),
-        FutureOrPresent::class to FutureOrPresentGenerator(),
-        Max::class to MaxGenerator(),
-        Min::class to MinGenerator(),
-        Negative::class to NegativeGenerator(),
-        NegativeOrZero::class to NegativeOrZeroGenerator(),
-        NotBlank::class to NotBlankGenerator(),
-        NotEmpty::class to NotEmptyGenerator(),
-        NotNull::class to NotNullGenerator(),
-        Null::class to NullGenerator(),
-        Past::class to PastGenerator(),
-        PastOrPresent::class to PastOrPresentGenerator(),
-        Pattern::class to PatternGenerator(),
-        Positive::class to PositiveGenerator(),
-        PositiveOrZero::class to PositiveOrZeroGenerator(),
-        Size::class to SizeGenerator()
+            AssertFalse::class to AssertFalseGenerator(),
+            AssertTrue::class to AssertTrueGenerator(),
+            DecimalMax::class to DecimalMaxGenerator(),
+            DecimalMin::class to DecimalMinGenerator(),
+            Digits::class to DigitsGenerator(),
+            Email::class to EmailGenerator(),
+            Future::class to FutureGenerator(),
+            FutureOrPresent::class to FutureOrPresentGenerator(),
+            Max::class to MaxGenerator(),
+            Min::class to MinGenerator(),
+            Negative::class to NegativeGenerator(),
+            NegativeOrZero::class to NegativeOrZeroGenerator(),
+            NotBlank::class to NotBlankGenerator(),
+            NotEmpty::class to NotEmptyGenerator(),
+            NotNull::class to NotNullGenerator(),
+            Null::class to NullGenerator(),
+            Past::class to PastGenerator(),
+            PastOrPresent::class to PastOrPresentGenerator(),
+            Pattern::class to PatternGenerator(),
+            Positive::class to PositiveGenerator(),
+            PositiveOrZero::class to PositiveOrZeroGenerator(),
+            Size::class to SizeGenerator()
     )
+
+    private val defaultPriority: Map<KClass<out Annotation>, Long> = mapOf(
+            AssertFalse::class to 0L,
+            AssertTrue::class to 0L,
+            DecimalMax::class to 0L,
+            DecimalMin::class to 0L,
+            Digits::class to 0L,
+            Email::class to 0L,
+            Future::class to 0L,
+            FutureOrPresent::class to 0L,
+            Max::class to 0L,
+            Min::class to 0L,
+            Negative::class to 0L,
+            NegativeOrZero::class to 0L,
+            NotBlank::class to -10L,
+            NotEmpty::class to -10L,
+            NotNull::class to -10L,
+            Null::class to -10L,
+            Past::class to 0L,
+            PastOrPresent::class to 0L,
+            Pattern::class to 0L,
+            Positive::class to 0L,
+            PositiveOrZero::class to 0L,
+            Size::class to 0L
+    )
+
+    private lateinit var usersGenerators: Map<KClass<out Annotation>, ValidationParamResolver>
 
     fun generate(parameterContext: ParameterContext, extensionContext: ExtensionContext): Any {
         val type = parameterContext.parameter.type
         return generateParam(type.kotlin, type.toKType(), null)
+                ?: throw RuntimeException("Something went wrong")
     }
 
-    private fun generateParam(clazz: KClass<*>, type: KType, annotation: Annotation?): Any {
+    private fun generateParam(clazz: KClass<*>, type: KType, annotation: List<Annotation>?): Any? {
         return if (clazz.isSimple()) {
             processSimpleType(clazz, type, annotation)
         } else {
             val constructor = clazz.constructors.toMutableList()[0]
             val arguments = constructor.parameters.map { param ->
                 val newAnnotation =
-                    clazz.java.declaredFields.firstOrNull { it.name == param.name }?.annotations?.getOrNull(0)
+                        clazz.java.declaredFields.firstOrNull { it.name == param.name }?.annotations?.toList()
                 generateParam(param.type.classifier as KClass<*>, param.type, newAnnotation)
             }.toTypedArray()
             constructor.call(*arguments)
         }
     }
 
-    private fun processSimpleType(clazz: KClass<*>, type: KType, annotation: Annotation?): Any {
-        return if (annotation == null) {
+    private fun processSimpleType(clazz: KClass<*>, type: KType, annotationList: List<Annotation>?): Any? {
+        return if (annotationList == null) {
             makeRandomInstance(
-                clazz,
-                type
-            )!!
+                    clazz,
+                    type
+            )
         } else {
-            if (annotation.annotationClass == ValidateAnnotation::class) {
-                TODO("тут будет код")
-            } else {
-                val generator = generators[annotation.annotationClass]
-                generator?.process(null, clazz, type, annotation)
-                    ?: throw NoSuchValidAnnotationException("Please annotate your validate annotation with ValidateAnnotation class")
+            val sorted = annotationList.sortedBy {
+                return@sortedBy defaultPriority[it.annotationClass]
+            }
+            var generatedParam: Any? = null
+            for (annotation in sorted) {
+                generatedParam = if (annotation.annotationClass == ValidateAnnotation::class) {
+                    val generator = usersGenerators[annotation.annotationClass]
+                            ?: throw NoSuchValidAnnotationException("Please annotate your validate annotation with ValidateAnnotation class")
+                    generator.process(generatedParam, clazz, type, annotation)
+                } else {
+                    val generator = generators[annotation.annotationClass]
+                            ?: throw NoSuchValidAnnotationException("Please annotate your validate annotation with ValidateAnnotation class")
+                    generator.process(generatedParam, clazz, type, annotation)
+
+                }
             }
         }
     }
