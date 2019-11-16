eb0d4175797b1aabf2dfed0e28056076f597e7c4
kostya05983 <kostya05983@mail.ru>
2019-11-16 16:42:44 +0700

    Fixture processor

--- src/main/kotlin/ru/kontur/spring/test/generator/processor/processors/FixtureProcessor.kt
+++ src/main/kotlin/ru/kontur/spring/test/generator/processor/processors/FixtureProcessor.kt
@@ -1,3 +1,51 @@
 package ru.kontur.spring.test.generator.processor.processors
 
-class FixtureProcessor
+import ru.kontur.spring.test.generator.api.ValidationConstructor
+import ru.kontur.spring.test.generator.exceptions.NoOptionalRecursiveException
+import ru.kontur.spring.test.generator.extensions.isSimple
+import ru.kontur.spring.test.generator.processor.AbstractGenerateProcessor
+import kotlin.random.Random
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
+/**
+ * @author Konstantin Volivach
+ */
+class FixtureProcessor(
+    private val constructors: Map<KClass<*>, ValidationConstructor<*>>
+) : AbstractGenerateProcessor() {
+
+    override fun generateParam(clazz: KClass<*>, type: KType, annotation: List<Annotation>?): Any? {
+        return when {
+            clazz.isSimple() -> generatePrimitiveValue(clazz, type, null)
+            clazz.java.isEnum -> {
+                val x = Random.nextInt(clazz.java.enumConstants.size)
+                clazz.java.enumConstants[x]
+            }
+            else -> {
+                when {
+                    constructors.containsKey(clazz) -> constructors[clazz]?.call()
+                    else -> {
+                        createClazz(clazz)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun createClazz(clazz: KClass<*>): Any {
+        val constructor = clazz.constructors.toMutableList()[0]
+        val arguments = constructor.parameters.map { param ->
+            val paramClazz = param.type.classifier as KClass<*>
+            if (paramClazz == clazz) {
+                if (param.isOptional) {
+                    return@map null
+                } else {
+                    throw NoOptionalRecursiveException("Recursive field can't be required")
+                }
+            }
+            generateParam(param.type.classifier as KClass<*>, param.type, null)
+        }.toTypedArray()
+        return constructor.call(*arguments)
+    }
+}
