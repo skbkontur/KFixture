2f59f13f83dfaa1af0b47d08bc4672adb2d08e66
kostya05983 <kostya05983@mail.ru>
2019-11-16 17:08:39 +0700

    New feature with fixture without use of annotations

--- src/main/kotlin/ru/kontur/spring/test/generator/resolver/strategy/FixtureResolverStrategy.kt
+++ src/main/kotlin/ru/kontur/spring/test/generator/resolver/strategy/FixtureResolverStrategy.kt
@@ -2,15 +2,37 @@ package ru.kontur.spring.test.generator.resolver.strategy
 
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.api.extension.ParameterContext
+import ru.kontur.spring.test.generator.api.SpringTestDataGenerator
 import ru.kontur.spring.test.generator.api.ValidationConstructor
+import ru.kontur.spring.test.generator.processor.GeneratorAnnotationScanner
+import ru.kontur.spring.test.generator.processor.processors.FixtureProcessor
 import ru.kontur.spring.test.generator.resolver.ResolverStrategy
+import ru.kontur.spring.test.generator.utils.toKType
+import java.lang.RuntimeException
 import kotlin.reflect.KClass
 
 class FixtureResolverStrategy(
     private val defaultConstructors: Map<KClass<*>, ValidationConstructor<*>>
 ) : ResolverStrategy {
+    private val generatorAnnotationScanner = GeneratorAnnotationScanner()
 
     override fun resolve(parameterContext: ParameterContext, extensionContext: ExtensionContext): Any {
-        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
+        val annotation = extensionContext.testInstance.get()::class.annotations.firstOrNull {
+            it is SpringTestDataGenerator
+        } as? SpringTestDataGenerator
+
+        val constructors = mutableMapOf<KClass<*>, ValidationConstructor<*>>()
+        constructors.putAll(defaultConstructors)
+
+        if (annotation != null) {
+            val usersConstructors = generatorAnnotationScanner.getConstructors(annotation.value)
+            constructors.putAll(usersConstructors)
+        }
+
+        val clazzProcessor = FixtureProcessor(constructors)
+
+        val type = parameterContext.parameter.type
+        return clazzProcessor.generateParam(type.kotlin, type.toKType(), null)
+            ?: throw RuntimeException("Something went wrong")
     }
 }
