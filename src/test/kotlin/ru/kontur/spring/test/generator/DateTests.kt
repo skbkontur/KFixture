e99baee9e792fcec3373909ccdf038609d7dd01e
kostya05983 <kostya05983@mail.ru>
2019-11-16 18:08:05 +0700

    Classify exception

--- src/test/kotlin/ru/kontur/spring/test/generator/DateTests.kt
+++ src/test/kotlin/ru/kontur/spring/test/generator/DateTests.kt
@@ -1,4 +1,27 @@
 package ru.kontur.spring.test.generator
 
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.extension.ExtendWith
+import ru.kontur.spring.test.generator.annotations.Fixture
+import ru.kontur.spring.test.generator.api.SpringTestDataGenerator
+import ru.kontur.spring.test.generator.resolver.FixtureParameterResolver
+import java.time.LocalDate
+import java.time.ZoneOffset
+import java.util.*
+
+@ExtendWith(FixtureParameterResolver::class)
+@SpringTestDataGenerator(value = "ru.kontur.spring.test.generator")
 class DateTests {
+
+    @RepeatedTest(100)
+    fun localDate(@Fixture localDate: LocalDate) {
+    }
+
+    @RepeatedTest(100)
+    fun date(@Fixture date: Date) {
+    }
+
+    @RepeatedTest(100)
+    fun zoneOffset(@Fixture zoneOffset: ZoneOffset) {
+    }
 }
