0f4288d3f4e3a8ca60e6db7d5d96f3ba1674fc4d
kostya05983 <kostya05983@mail.ru>
2019-12-23 00:15:02 +0700

    Finish with decimal.

---
+++ library/src/main/kotlin/ru/kontur/kinfra/kfixture/generators/decimal/MinIntGenerator.kt
@@ -0,0 +1,17 @@
+package ru.kontur.kinfra.kfixture.generators.decimal
+
+import javax.validation.constraints.DecimalMin
+
+/**
+ * @author Konstantin Volivach
+ */
+class MinIntGenerator {
+    fun process(param: Int?, decimalMin: DecimalMin): Int {
+        val min = decimalMin.value.toInt()
+        return if (param == null || param < min) {
+            min + 1
+        } else {
+            param
+        }
+    }
+}
---
+++ library/src/main/kotlin/ru/kontur/kinfra/kfixture/generators/decimal/MinShortGenerator.kt
@@ -0,0 +1,17 @@
+package ru.kontur.kinfra.kfixture.generators.decimal
+
+import javax.validation.constraints.DecimalMin
+
+/**
+ * @author Konstantin Volivach
+ */
+class MinShortGenerator {
+    fun process(param: Short?, decimalMin: DecimalMin): Short {
+        val min = decimalMin.value.toInt()
+        return if (param == null || param < min) {
+            (min + 1).toShort()
+        } else {
+            param
+        }
+    }
+}
