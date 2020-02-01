ee857a1efed6644bb4167d06837fdc71db32ee93
kostya05983 <kostya05983@mail.ru>
2020-02-02 02:05:33 +0700

    Rewrite generators for new annotation.

--- library/src/main/kotlin/ru/kontur/kinfra/kfixture/generators/NullGenerator.kt
+++ library/src/main/kotlin/ru/kontur/kinfra/kfixture/generators/NullGenerator.kt
@@ -1,14 +1,10 @@
 package ru.kontur.kinfra.kfixture.generators
 
-import ru.kontur.kinfra.kfixture.api.ValidationParamResolver
-import ru.kontur.kinfra.kfixture.api.ResolverFor
+import ru.kontur.kinfra.kfixture.api.ValidParamGenerator
 import javax.validation.constraints.Null
-import kotlin.reflect.KClass
-import kotlin.reflect.KType
 
-@ResolverFor(value = Null::class)
-class NullGenerator : ValidationParamResolver {
-    override fun <T> process(generatedParam: T?, clazz: KClass<*>, type: KType, annotation: Annotation): Any? {
-        return null
+class NullGenerator<T> : ValidParamGenerator<T, Null> {
+    override fun process(param: T, annotation: Null): T {
+        TODO("not implemented") // хмм
     }
 }
