e6512e0e5d54c8e8112e23c327f1b2a2c73dee0f
kostya05983 <kostya05983@mail.ru>
2020-02-02 01:36:37 +0700

    Min Router impl.

--- library/src/main/kotlin/ru/kontur/kinfra/kfixture/generators/decimal/MinDecimalGenerator.kt
+++ library/src/main/kotlin/ru/kontur/kinfra/kfixture/generators/decimal/MinDecimalGenerator.kt
@@ -2,12 +2,12 @@ package ru.kontur.kinfra.kfixture.generators.decimal
 
 import ru.kontur.kinfra.kfixture.api.ValidParamGenerator
 import ru.kontur.kinfra.kfixture.generators.VariableCreator
-import ru.kontur.kinfra.kfixture.generators.operators.PlusSupplyer
+import ru.kontur.kinfra.kfixture.generators.operators.PlusSupplier
 import javax.validation.constraints.DecimalMin
 
 class MinDecimalGenerator<T : Comparable<T>>(
     private val creator: VariableCreator<T>,
-    private val plusSupplyer: PlusSupplyer<T>
+    private val plusSupplyer: PlusSupplier<T>
 ) : ValidParamGenerator<T, DecimalMin> {
 
     override fun process(param: T, annotation: DecimalMin): T {
