16e21258509c0a5264c5a565968d125b2ee7382b
kostya05983 <kostya05983@mail.ru>
2020-02-04 15:06:02 +0700

    Refactor parameterResolver exceptions

--- library/src/main/kotlin/ru/kontur/kinfra/kfixture/resolver/strategy/JavaxFixtureResolverStrategy.kt
+++ library/src/main/kotlin/ru/kontur/kinfra/kfixture/resolver/strategy/JavaxFixtureResolverStrategy.kt
@@ -2,11 +2,11 @@ package ru.kontur.kinfra.kfixture.resolver.strategy
 
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.api.extension.ParameterContext
+import ru.kontur.kinfra.kfixture.exceptions.FixtureGenerationException
 import ru.kontur.kinfra.kfixture.processor.GeneratorAnnotationScanner
 import ru.kontur.kinfra.kfixture.processor.processors.JavaxFixtureProcessor
 import ru.kontur.kinfra.kfixture.resolver.ResolverStrategy
 import ru.kontur.kinfra.kfixture.utils.toKType
-import java.lang.RuntimeException
 
 class JavaxFixtureResolverStrategy(
     private val generatorAnnotationScanner: GeneratorAnnotationScanner
@@ -21,6 +21,6 @@ class JavaxFixtureResolverStrategy(
 
         val type = parameterContext.parameter.type
         return classProcessor.generateParam(type.kotlin, type.toKType(), null)
-            ?: throw RuntimeException("Something went wrong")
+            ?: throw FixtureGenerationException(type.typeName, parameterContext.parameter.name)
     }
 }
