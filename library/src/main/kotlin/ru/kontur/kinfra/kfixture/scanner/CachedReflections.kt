15dc092afdebde7073bb3f87eb2e116e448f389a
kostya05983 <kostya05983@mail.ru>
2020-06-09 01:47:00 +0700

    Add future router.

--- library/src/main/kotlin/ru/kontur/kinfra/kfixture/scanner/CachedReflections.kt
+++ library/src/main/kotlin/ru/kontur/kinfra/kfixture/scanner/CachedReflections.kt
@@ -13,10 +13,12 @@ class CachedReflections {
             return if (stored.pathes.containsAll(paths)) {
                 stored.reflections
             } else {
-                val wrapper = ReflectionsWrapper(paths + listOf(
-                    LIBRARY_PATH,
-                    JAVAX_PATH
-                ))
+                val wrapper = ReflectionsWrapper(
+                    paths + listOf(
+                        LIBRARY_PATH,
+                        JAVAX_PATH
+                    )
+                )
                 extensionContext.getStore(ExtensionContext.Namespace.GLOBAL).put(DEFAULT_REFLECTION_KEY, wrapper)
                 wrapper.reflections
             }
@@ -24,10 +26,12 @@ class CachedReflections {
             return if (extensionContext.parent.isPresent) {
                 getReflections(paths, extensionContext.parent.get())
             } else {
-                val wrapper = ReflectionsWrapper(paths + listOf(
-                    LIBRARY_PATH,
-                    JAVAX_PATH
-                ))
+                val wrapper = ReflectionsWrapper(
+                    paths + listOf(
+                        LIBRARY_PATH,
+                        JAVAX_PATH
+                    )
+                )
                 extensionContext.getStore(ExtensionContext.Namespace.GLOBAL).put(DEFAULT_REFLECTION_KEY, wrapper)
                 wrapper.reflections
             }
@@ -36,7 +40,7 @@ class CachedReflections {
 
     private companion object {
         const val DEFAULT_REFLECTION_KEY = "REFLECTIONS_REFERENCES"
-        const val LIBRARY_PATH = "ru.kontur.test.kfixture"
+        const val LIBRARY_PATH = "ru.kontur.kinfra.kfixture"
         const val JAVAX_PATH = "javax.validation.constraints"
     }
 }
