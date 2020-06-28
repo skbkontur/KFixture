69be1a3343dc59eb47d1ee732028cdf5a52ea269
kostya05983 <kostya05983@mail.ru>
2020-06-29 01:35:47 +0700

    Add not null javax test.

---
+++ library/src/test/kotlin/ru/kontur/kinfra/kfixture/functional/javax/NotNullJavaxTest.kt
@@ -0,0 +1,22 @@
+package ru.kontur.kinfra.kfixture.functional.javax
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.extension.ExtendWith
+import ru.kontur.kinfra.kfixture.annotations.JavaxFixture
+import ru.kontur.kinfra.kfixture.resolver.FixtureParameterResolver
+import javax.validation.constraints.NotNull
+
+@ExtendWith(FixtureParameterResolver::class)
+private class NotNullJavaxTest {
+
+    data class NotNullWrapper(
+        @field:NotNull
+        val param: String?
+    )
+
+    @RepeatedTest(10)
+    fun `should generate not null value`(@JavaxFixture fixture: NotNullWrapper) {
+        assertEquals(fixture.param != null, true)
+    }
+}
