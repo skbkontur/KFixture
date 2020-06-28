e7ea2198330683e1ab6331c841beb3a780787c65
kostya05983 <kostya05983@mail.ru>
2020-06-28 17:55:18 +0700

    Add negative or zero test.

---
+++ library/src/main/kotlin/ru/kontur/kinfra/kfixture/routers/NegativeOrZeroRouter.kt
@@ -0,0 +1,36 @@
+package ru.kontur.kinfra.kfixture.routers
+
+import ru.kontur.kinfra.kfixture.exceptions.AnnotationCantBeAppliedException
+import ru.kontur.kinfra.kfixture.generators.NegativeOrZeroGenerator
+import ru.kontur.kinfra.kfixture.generators.creators.*
+import java.math.BigDecimal
+import java.math.BigInteger
+import javax.validation.constraints.NegativeOrZero
+import kotlin.reflect.KClass
+import kotlin.reflect.KType
+
+@Suppress("unused")
+class NegativeOrZeroRouter<T> : ValidRouter<T, NegativeOrZero> where T : Any {
+    private val bigDecimalNegativeOrZeroGenerator = NegativeOrZeroGenerator(BigDecimalCreator())
+    private val bigIntegerNegativeOrZeroGenerator = NegativeOrZeroGenerator(BigIntegerCreator())
+    private val byteNegativeOrZeroGenerator = NegativeOrZeroGenerator(ByteCreator())
+    private val shortNegativeOrZeroGenerator = NegativeOrZeroGenerator(ShortCreator())
+    private val intNegativeOrZeroGenerator = NegativeOrZeroGenerator(IntCreator())
+    private val longNegativeOrZeroGenerator = NegativeOrZeroGenerator(LongCreator())
+    private val floatNegativeOrZeroGenerator = NegativeOrZeroGenerator(FloatCreator())
+    
+    override fun process(param: T, annotation: NegativeOrZero, clazz: KClass<*>, type: KType): Any? {
+        return when (param) {
+            is BigDecimal -> bigDecimalNegativeOrZeroGenerator.process(param, annotation, clazz, type)
+            is BigInteger -> bigIntegerNegativeOrZeroGenerator.process(param, annotation, clazz, type)
+            is Byte -> byteNegativeOrZeroGenerator.process(param, annotation, clazz, type)
+            is Short -> shortNegativeOrZeroGenerator.process(param, annotation, clazz, type)
+            is Int -> intNegativeOrZeroGenerator.process(param, annotation, clazz, type)
+            is Long -> longNegativeOrZeroGenerator.process(param, annotation, clazz, type)
+            is Float -> floatNegativeOrZeroGenerator.process(param, annotation, clazz, type)
+            else -> {
+                throw AnnotationCantBeAppliedException(annotation, clazz)
+            }
+        }
+    }
+}
---
+++ library/src/test/kotlin/ru/kontur/kinfra/kfixture/functional/javax/NegativeOrZeroJavaxTest.kt
@@ -0,0 +1,73 @@
+package ru.kontur.kinfra.kfixture.functional.javax
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.extension.ExtendWith
+import ru.kontur.kinfra.kfixture.annotations.JavaxFixture
+import ru.kontur.kinfra.kfixture.resolver.FixtureParameterResolver
+import java.math.BigDecimal
+import java.math.BigInteger
+import javax.validation.constraints.NegativeOrZero
+
+@ExtendWith(FixtureParameterResolver::class)
+private class NegativeOrZeroJavaxTest {
+    data class BigDecimalWrapper(
+        @field:NegativeOrZero
+        val value: BigDecimal
+    )
+
+    @RepeatedTest(10)
+    fun `should generate big decimal negative or zero`(@JavaxFixture wrapper: BigDecimalWrapper) {
+        Assertions.assertEquals(wrapper.value <= BigDecimal.ZERO, true)
+    }
+
+    data class BigIntegerWrapper(
+        @field:NegativeOrZero
+        val value: BigInteger
+    )
+
+    @RepeatedTest(10)
+    fun `should generate big integer negative or zero`(@JavaxFixture wrapper: BigIntegerWrapper) {
+        Assertions.assertEquals(wrapper.value <= BigInteger.ZERO, true)
+    }
+
+    data class LongWrapper(
+        @field:NegativeOrZero
+        val value: Long
+    )
+
+    @RepeatedTest(10)
+    fun `should generate long negative or zero`(@JavaxFixture wrapper: LongWrapper) {
+        Assertions.assertEquals(wrapper.value <= 0, true)
+    }
+
+    data class IntWrapper(
+        @field:NegativeOrZero
+        val value: Int
+    )
+
+    @RepeatedTest(10)
+    fun `should generate int negative or zero`(@JavaxFixture wrapper: IntWrapper) {
+        Assertions.assertEquals(wrapper.value <= 0, true)
+    }
+
+    data class ShortWrapper(
+        @field:NegativeOrZero
+        val value: Short
+    )
+
+    @RepeatedTest(10)
+    fun `should generate short negative or zero`(@JavaxFixture wrapper: ShortWrapper) {
+        Assertions.assertEquals(wrapper.value <= 0, true)
+    }
+
+    data class ByteWrapper(
+        @field:NegativeOrZero
+        val value: Byte
+    )
+
+    @RepeatedTest(10)
+    fun `should generate byte negative or zero`(@JavaxFixture wrapper: ByteWrapper) {
+        Assertions.assertEquals(wrapper.value <= 0, true)
+    }
+}
