de4c6297bca96e9ccf55ad94db66ff61098418fb
kostya05983 <kostya05983@mail.ru>
2020-06-29 01:29:11 +0700

    Add not blank tests.

---
+++ library/src/test/kotlin/ru/kontur/kinfra/kfixture/functional/javax/NotBlankJavaxTest.kt
@@ -0,0 +1,21 @@
+package ru.kontur.kinfra.kfixture.functional.javax
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.extension.ExtendWith
+import ru.kontur.kinfra.kfixture.annotations.JavaxFixture
+import ru.kontur.kinfra.kfixture.resolver.FixtureParameterResolver
+import javax.validation.constraints.NotBlank
+
+@ExtendWith(FixtureParameterResolver::class)
+private class NotBlankJavaxTest {
+    data class NotBlankWrapper(
+        @field:NotBlank
+        val param: String
+    )
+
+    @RepeatedTest(10)
+    fun `should generate not blank string`(@JavaxFixture fixture: NotBlankWrapper) {
+        assertEquals(fixture.param.isNotBlank(), true)
+    }
+}
