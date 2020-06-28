4e82e968230a9b427b6ae9637ebc7801f0b1bd75
kostya05983 <kostya05983@mail.ru>
2020-06-29 01:49:26 +0700

    Add pattern javax test.

---
+++ library/src/test/kotlin/ru/kontur/kinfra/kfixture/functional/javax/PatternJavaxTest.kt
@@ -0,0 +1,23 @@
+package ru.kontur.kinfra.kfixture.functional.javax
+
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.extension.ExtendWith
+import ru.kontur.kinfra.kfixture.annotations.JavaxFixture
+import ru.kontur.kinfra.kfixture.resolver.FixtureParameterResolver
+import javax.validation.constraints.Pattern
+
+@ExtendWith(FixtureParameterResolver::class)
+private class PatternJavaxTest {
+    data class PatternWrapper(
+        @field:Pattern(regexp = "\\d{10}")
+        val param: String
+    )
+
+    @RepeatedTest(10)
+    fun `should generate digits`(@JavaxFixture fixture: PatternWrapper) {
+        assertDoesNotThrow {
+            fixture.param.toBigInteger()
+        }
+    }
+}
