34caee395e35917d4f0dbf5acc6e3e7d290d9d3b
kostya05983 <kostya05983@mail.ru>
2020-06-29 01:40:38 +0700

    Add past javax test.

--- library/src/main/kotlin/ru/kontur/kinfra/kfixture/generators/dates/MonthDayDateCreator.kt
+++ library/src/main/kotlin/ru/kontur/kinfra/kfixture/generators/dates/MonthDayDateCreator.kt
@@ -5,7 +5,7 @@ import java.time.MonthDay
 class MonthDayDateCreator : DateCreator<MonthDay> {
     override fun create(interval: TimeInterval): MonthDay {
         return when (interval) {
-            TimeInterval.PAST -> MonthDay.now().withMonth(0)
+            TimeInterval.PAST -> MonthDay.now().withMonth(1)
             TimeInterval.NOW -> MonthDay.now()
             TimeInterval.FUTURE -> MonthDay.now().withMonth(12)
         }
