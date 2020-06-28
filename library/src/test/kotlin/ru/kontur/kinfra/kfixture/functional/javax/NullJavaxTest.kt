fdf4799e0e56ba92479b25c7dc29faea64669ce6
kostya05983 <kostya05983@mail.ru>
2020-06-29 01:38:11 +0700

    Add null test.

---
+++ library/src/test/kotlin/ru/kontur/kinfra/kfixture/functional/javax/NullJavaxTest.kt
@@ -0,0 +1,22 @@
+package ru.kontur.kinfra.kfixture.functional.javax
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.RepeatedTest
+import org.junit.jupiter.api.extension.ExtendWith
+import ru.kontur.kinfra.kfixture.annotations.JavaxFixture
+import ru.kontur.kinfra.kfixture.resolver.FixtureParameterResolver
+import javax.validation.constraints.Null
+
+@ExtendWith(FixtureParameterResolver::class)
+private class NullJavaxTest {
+
+    data class NullWrapper(
+        @field:Null
+        val param: String?
+    )
+
+    @RepeatedTest(10)
+    fun `should generate null param`(@JavaxFixture fixture: NullWrapper) {
+        assertEquals(fixture.param == null, true)
+    }
+}
