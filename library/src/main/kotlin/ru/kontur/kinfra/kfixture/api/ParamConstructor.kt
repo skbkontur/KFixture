6a43d791fb551f715ad20ee79614696ecc72378d
kostya05983 <kostya05983@mail.ru>
2020-07-02 02:18:42 +0700

    Add docs for generation

--- library/src/main/kotlin/ru/kontur/kinfra/kfixture/api/ParamConstructor.kt
+++ library/src/main/kotlin/ru/kontur/kinfra/kfixture/api/ParamConstructor.kt
@@ -1,5 +1,10 @@
 package ru.kontur.kinfra.kfixture.api
 
+/**
+ * Use to constructor difficult param
+ * if kfixture can't construct them automatically
+ * you can provide construction by self
+ */
 interface ParamConstructor<T : Any> {
     fun call(): T
 }
--- library/src/main/kotlin/ru/kontur/kinfra/kfixture/routers/ValidRouter.kt
+++ library/src/main/kotlin/ru/kontur/kinfra/kfixture/routers/ValidRouter.kt
@@ -3,6 +3,9 @@ package ru.kontur.kinfra.kfixture.routers
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
 
+/**
+ * Router is used to provide generation algorithm for your valid annotations
+ */
 interface ValidRouter<T : Any, A> {
     fun process(param: T, annotation: A, clazz: KClass<*>, type: KType): Any?
 }
