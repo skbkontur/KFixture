91e2a2edd4689cc31c56f7d67870206b12d12ae8
kostya05983 <kostya05983@mail.ru>
2020-09-02 00:25:27 +0700

    Stylish

--- library/src/test/kotlin/ru/kontur/kinfra/kfixture/TestGenerator.kt
+++ library/src/test/kotlin/ru/kontur/kinfra/kfixture/TestGenerator.kt
@@ -1,6 +1,5 @@
 package ru.kontur.kinfra.kfixture
 
-import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
