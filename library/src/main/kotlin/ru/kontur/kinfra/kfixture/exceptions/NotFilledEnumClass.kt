c4478a924a685dcb2c795921a11c2ddf94bbc25b
kubitre <kubitre@gmail.com>
2020-11-05 18:03:51 +0700

    fxied ktlint issue

--- library/src/main/kotlin/ru/kontur/kinfra/kfixture/exceptions/NotFilledEnumClass.kt
+++ library/src/main/kotlin/ru/kontur/kinfra/kfixture/exceptions/NotFilledEnumClass.kt
@@ -4,4 +4,4 @@ import java.lang.Exception
 
 class NotFilledEnumClass(
     className: String
-): Exception("Class $className not filled. At least once should have 1 constant in your enum")
+) : Exception("Class $className not filled. At least once should have 1 constant in your enum")
