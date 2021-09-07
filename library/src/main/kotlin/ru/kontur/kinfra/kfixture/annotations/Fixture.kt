f72a35f3fc2d1a57f213cf0a35c2b6d5b371fed9
kostya05983 <kostya05983@mail.ru>
2021-09-07 23:58:27 +0700

    Update library

--- library/src/main/kotlin/ru/kontur/kinfra/kfixture/annotations/Fixture.kt
+++
@@ -1,4 +0,0 @@
-package ru.kontur.kinfra.kfixture.annotations
-
-@Deprecated("Should use Fixture from ru.kontur.kinfra.kfixtur.api", ReplaceWith("Fixture", "ru.kontur.kinfra.kfixture.api.Fixture"))
-annotation class Fixture
--- library/src/main/kotlin/ru/kontur/kinfra/kfixture/annotations/JavaxFixture.kt
+++
@@ -1,7 +0,0 @@
-package ru.kontur.kinfra.kfixture.annotations
-
-@Deprecated(
-    "Should use JavaxFixture from ru.kontur.kinfra.kfixtur.api",
-    ReplaceWith("JavaxFixture", "ru.kontur.kinfra.kfixture.api.JavaxFixture")
-)
-annotation class JavaxFixture
